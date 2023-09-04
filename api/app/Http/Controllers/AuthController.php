6abf9d5fd8537a7a89caaed899677541696212bc
Thiago <contact.thiago.ferreira@gmail.com>
2023-09-04 09:51:22 +0200

    rebuilding auth

--- api/app/Http/Controllers/AuthController.php
+++ api/app/Http/Controllers/AuthController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
-    public function login(Request $request)
+    public function login(Request $request, Schedule $schedule)
     {
-
+      
 
         $fields = $request->validate([
             'email' => 'required|max:255',
@@ -26,7 +27,7 @@ class AuthController extends Controller
         if (!$user || !Hash::check($fields['password'], $user->password)) {
             return $this->error('', 'Credenciales errónea', 401);
         }
-
+        $schedule->command('sanctum:prune-expired --hours=24')->daily();
         return $this->success([
 
             'user' => $user,
@@ -40,5 +41,8 @@ class AuthController extends Controller
     public function logout(Request $resquest)
     {
         Auth::user()->currentAccessToken()->delete();
+        return  response()->json([
+            'message' => 'token deleted successfully'
+        ]);
     }
 }
